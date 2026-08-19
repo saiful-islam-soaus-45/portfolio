0e2bd5120e73723f560fc5cc491be98024a56e0c
saiful-islam-soaus-45 <soausahmedbd91@gmail.com>
2026-08-19 14:24:01 +0600

    Fixed some problem

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,14 +21,13 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
         <Providers>
-          {children}
+        {children}
         </Providers>
-      </body>
+        </body>
     </html>
   );
 }
